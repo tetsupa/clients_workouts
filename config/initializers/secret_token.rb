15f932a4d1819f7b3b8720e3abf7fb6c4b1f113c
Tetsuharu Kohkaki <tetsupa0427@gmail.com>
2011-07-10 17:39:38 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClientWorkouts::Application.config.secret_token = '61bcada316bafc3765cbfb3f76bf54bb50e6bbdf94d9debf76eae8436c39a1ad0b6515b0461e888166006526df07a97c7c6f7c3333e64ad34567d9ad8a7a7810'
