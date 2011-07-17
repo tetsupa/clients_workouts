e56b3d26071c1aa824a63c09ab5b8cc532d875d5
Tetsuharu Kohkaki <tetsupa0427@gmail.com>
2011-07-17 12:12:54 +0900

    validates presence of client name & trainer

--- app/models/client_workout.rb
+++ app/models/client_workout.rb
@@ -1,5 +1,5 @@
 class ClientWorkout < ActiveRecord::Base
   validates_numericality_of :paid_amount
-  validates_presense_of :client_name
-  validates_presense_of :trainer_name
+  validates_presence_of :client_name
+  validates_presence_of :trainer
 end
